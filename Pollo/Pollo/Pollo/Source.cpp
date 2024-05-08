c9370f3a09f0746f2d7fe555f99705e49b25645d
Dante <126302786+DanteaExe@users.noreply.github.com>
2024-05-08 11:20:59 -0600

    A beatiful chicken has been made
    UwU

--- Pollo/Pollo/Pollo/Source.cpp
+++ Pollo/Pollo/Pollo/Source.cpp
@@ -2,9 +2,46 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(600, 600), "Pollo");
+
+    // Crear objetos
+    sf::CircleShape HeadChicken(100.f);
+    sf::CircleShape Eye1(15);
+    sf::CircleShape Eye2(15);
+    sf::ConvexShape triangle(3);
+
+    // Dar colores
+    HeadChicken.setFillColor(sf::Color::Yellow);
+    Eye1.setFillColor(sf::Color::Blue);
+    Eye2.setFillColor(sf::Color::Blue);
+    triangle.setFillColor(sf::Color::Red);
+
+    // Obtener tamaño de la ventana
+    sf::Vector2u windowSize = window.getSize();
+
+    // Calcular posición para centrar HeadChicken
+    float xPos = (windowSize.x - HeadChicken.getRadius() * 2) / 2;
+    float yPos = (windowSize.y - HeadChicken.getRadius() * 2) / 2;
+
+    // Calcular posición para Eye1
+    float xEyePos = xPos - Eye1.getRadius() * -3;
+    float yEyePos = yPos - Eye1.getRadius() * -3;
+
+    // Calcular posición para Eye2
+    float xEye2Pos = xPos - Eye2.getRadius() * -8;
+    float yEye2Pos = yPos - Eye2.getRadius() * -3;
+
+    // Definir vértices del triángulo invertido
+    float triangleWidth = 40.f;
+    float triangleHeight = 30.f;
+    triangle.setPoint(0, sf::Vector2f(xPos, yPos + 90)); // Vértice inferior
+    triangle.setPoint(1, sf::Vector2f(xPos - triangleWidth / 2, yPos + 40)); // Vértice izquierdo
+    triangle.setPoint(2, sf::Vector2f(xPos + triangleWidth / 2, yPos + 40)); // Vértice derecho
+
+    // Posicionar objetos
+    HeadChicken.setPosition(xPos, yPos);
+    Eye1.setPosition(xEyePos, yEyePos);
+    Eye2.setPosition(xEye2Pos, yEye2Pos);
 
     while (window.isOpen())
     {
@@ -16,7 +53,10 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(HeadChicken);
+        window.draw(Eye1);
+        window.draw(Eye2);
+        window.draw(triangle);
         window.display();
     }
 
